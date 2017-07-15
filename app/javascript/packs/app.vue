150abe31860463010090c5d397a5660830f72162
Tom Brown <herestomwiththeweather@gmail.com>
2017-07-15 14:54:16 -0500

    hello_vue code leftover from last weekend

---
+++ app/javascript/packs/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <input v-model="message"><br />
+    <p>{{ message }}</p>
+    <ul>
+      <li v-for="food in food_items">{{food}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      food_items: ['hamburger','hotdog','fries']
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
