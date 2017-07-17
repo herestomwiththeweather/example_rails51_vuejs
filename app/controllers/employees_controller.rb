7e0c93ef4a04f832dac75824c4f3a57a076e128c
Tom Brown <herestomwiththeweather@gmail.com>
2017-07-17 00:57:12 -0500

    update employee row

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -45,12 +45,12 @@ class EmployeesController < ApplicationController
   # PATCH/PUT /employees/1.json
   def update
     respond_to do |format|
-      if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
-      else
-        format.html { render :edit }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      format.json do
+        if @employee.update(employee_params)
+          render :json => @employee
+        else
+          render :json => { :errors => @employee.errors.messages }, :status => 422
+        end
       end
     end
   end
--- app/javascript/components/employee_row.vue
+++ app/javascript/components/employee_row.vue
@@ -1,8 +1,30 @@
 <template>
 <tr>
-  <td>{{employee.name}}</td>
-  <td>{{employee.email}}</td>
-  <td>{{employee.manager}}</td>
+  <td>
+    <div v-if="editMode">
+      <input type="text" v-model="employee.name"><br>
+      <span style="color:red">{{ errors.name }}</span>
+    </div>
+    <div v-else>{{ employee.name }}</div>
+  </td>
+  <td>
+    <div v-if="editMode">
+      <input type="text" v-model="employee.email"><br>
+      <span style="color:red">{{ errors.name }}</span>
+    </div>
+    <div v-else>{{ employee.email }}</div>
+  </td>
+  <td>
+    <div v-if="editMode">
+      <input type="checkbox" v-model="employee.manager">
+    </div>
+    <div v-else>{{ employee.manager ? '&#10004;' : '' }}</div>
+  </td>
+  <td>
+    <button v-if="editMode" @click="updateEmployee">Save</button>
+    <button v-else @click="editMode = true">Edit</button>
+    <button v-if="!editMode" @click="toggleManagerStatus">{{ employee.manager ? 'Demote' : 'Promote' }}</button>
+  </td>
 </tr>
 </template>
 <script>
@@ -10,6 +32,36 @@ export default {
   name: 'employee-row',
   props: {
     employee: Object
+  },
+  data: function () {
+    return {
+      editMode: false,
+      errors: {}
+    }
+  },
+  methods: {
+    toggleManagerStatus: function () {
+      this.employee.manager = !this.employee.manager;
+      this.updateEmployee();
+    },
+    updateEmployee: function () {
+      var that = this;
+      $.ajax({
+	method: 'PUT',
+	data: {
+	  employee: that.employee
+	},
+	url: '/employees/' + that.employee.id + '.json',
+	success: function(res) {
+	  that.errors = {};
+	  that.employee = res;
+	  that.editMode = false;
+	},
+	error: function(res) {
+	  that.errors = res.responseJSON.errors;
+	}
+      });
+    }
   }
 }
 </script>
